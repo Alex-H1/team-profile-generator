ff7ab86ed488878f03634485d9f9a643e7c52eaf
alex hernandez <alexanderohernandez21@gmail.com>
2022-04-25 18:59:03 -0700

    added intern test

---
+++ test/intern.test.js
@@ -0,0 +1,12 @@
+// const intern = require('../lib/intern')
+
+describe('intern class',()=>{
+    describe('initializing',()=>{
+        it('should create a card with name and id',()=>{
+            const intern = new intern('steve','387');
+
+            expect(intern.name).toEqual('steve');
+            expect(intern.id).toEqual('387');
+        })
+    })
+})
