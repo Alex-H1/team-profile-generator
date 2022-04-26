522f9a891d7e54d61dbd75afcec637f3929a4c57
alex hernandez <alexanderohernandez21@gmail.com>
2022-04-25 17:06:25 -0700

    added engineer prompts

--- index.js
+++ index.js
@@ -1,42 +1,113 @@
 //install packacges
 const inquierer = require('inquirer');
 const fs = require('fs');
+const { mainModule } = require('process');
+employees =[];
 
-team =[];
-// make inquierer prompts 
+// make inquierer prompts
+
+// make prompts for employee 
+// prompt for engineer 
+// when selected engineer prompt for name, id, email, GitHub username
+function engineerQ(){
+    inquierer.prompt([
+        {
+            name: 'name',
+            message: 'what is your engineer\'s name?'
+        },
+        {
+            name: 'id',
+            message: 'what is your ingeneer\'s id?'
+        },
+        {
+            name: 'email',
+            message: 'what is your engineer\'s email address?'
+        },
+        {
+            name: 'github',
+            message: 'what is your engineer\'s GitHub username'
+        },
+        {
+            type: 'list',
+            name: 'addEmp',
+            message: 'do you want to add another employee?',
+            choices: ['intern','engineer','none']
+        }
+    ])
+    .then((engineerAns)=>{
+        const engineer = new Engineer(engineerAns.name, engineerAns.id, engineerAns.email, engineerAns.github)
+        employees.push(engineer);
+        switch(engineerAns.addEmp){
+            case 'engineer':
+                engineerQ();
+                break;
+            case 'intern':
+                internQ();
+                break;
+            default:
+                writeToFile();
+        }
+    })
+} 
+
+// prompt for intern
+//  when selected intern prompt for name, id, email, school, then taken back to menu
+
+// prompt for manager 
+// prompt for managers name, employee id, email, and office number
 const managerQ = () =>{
     inquierer.prompt([
     {
         name: 'name',
-        message: 'What is your manager/s name?'
+        message: 'What is your manager\'s name?'
     },    
     {
         name: 'id',
-        message: 'what is your managers/s id?'
+        message: 'what is your managers\`s id?'
     },
     {
         name: 'email',
-        message: 'what is your manager/s email address?'
+        message: 'what is your manager\'s email address?'
     },
     {
         name: 'phone',
         message: 'what is your managers office phone number?'
     },
+    {
+        type: 'list',
+        name: 'addEmp',
+        message: 'do you want to add another employee?',
+        choices: ['intern','engineer','none']
+    }
     ])
+    .then((managerAns)=>{
+        const manager = new Manager(managerAns.name, managerAns.id, managerAns.email, managerAns.phone)
+        employees.push(manager);
+        console.log(employees)
+        switch(managerAns.addEmp){
+            case 'engineer':
+                engineerQ();
+                break;
+            case 'intern':
+                internQ();
+                break;
+            default:
+                writeToFile();
+        }
+    })
 }
 
-// make prompts for employee 
-// prompt for engineer 
-// prompt for intern
-// prompt for manager 
 
-// prompt for managers name, employee id, email, and office number
-// then prompted for option to add engineer intern or finish building team 
-// when selected engineer prompt for name, id, email, GitHub username, then taken back to menu
-//  when selected intern prompt for name, id, email, school, then taken back to menu
+
 
 
 // make tests employee
 // make test for engineer 
 // make test for intern
 // make test for manager 
+
+function init(){
+    managerQ();
+}
+
+init();
