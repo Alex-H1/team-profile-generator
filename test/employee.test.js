747474b2eeee93c70a6e04dfa03b6d032e295d5c
alex hernandez <alexanderohernandez21@gmail.com>
2022-04-25 19:39:20 -0700

    added employee test

---
+++ test/employee.test.js
@@ -0,0 +1,12 @@
+// const employee = require('../lib/employee')
+
+describe('employee class',()=>{
+    describe('initializing',()=>{
+        it('should create a new card with name and id',()=>{
+            const employee = new employee('alex','234');
+
+            expect(employee.name).toEqual('alex');
+            expect(employee.id).toEqual('234');
+        })
+    })
+})
